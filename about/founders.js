8ae975472c2d2730f58a6ccb402cb1b8d330d1f8
Ghufran Ataie <ghufranataie@hotmail.com>
2026-03-06 17:27:09 -0500

    Changes to Founder

--- about/founders.js
+++ about/founders.js
@@ -1,18 +1,23 @@
 const getDBConnection = require('../config/db');
 
-exports.getFounders = async (event) => {
+exports.getFounders = async () => {
     try {
         const db = await getDBConnection();
         const [rows] = await db.execute("SELECT * FROM founders");
+
         return {
             statusCode: 200,
             body: JSON.stringify(rows)
         };
+
     } catch (error) {
-        console.error("DB Error:", error);
+        console.error("DATABASE ERROR:", error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ message: "Database error" })
+            body: JSON.stringify({
+                message: "Database error",
+                error: error.message
+            })
         };
     }
-}
+};
